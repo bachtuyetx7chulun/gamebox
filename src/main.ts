423c75dfaa5e8531c3461dd3728a58697cf00449
DESKTOP-3FRC7CU\FABBI <fabbi@fabbi.io>
2021-07-22 17:52:28 +0700

    chore: add validationPipe

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from '@app/app.module'
 import { ConfigService } from '@nestjs/config'
 import * as chalk from 'chalk'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe({}))
   const port = app.get(ConfigService).get('HOST_PORT')
   await app.listen(port, () => {
     console.log(`${chalk.green('Server is running on port')} ${chalk.yellow(port)} `)
